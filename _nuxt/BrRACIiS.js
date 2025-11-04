6379e913b85d31535f7b7302f4d201e41ea8c6a8
Olamide-png <otoyinbo@eshopworld.com>
2025-11-04 15:19:23 +0000

    Deploying to gh-pages from @ Olamide-png/docs-test@25b7a7668ead2254ee0fea9cfbd5b1fb46cfa863 🚀

---
+++ _nuxt/BrRACIiS.js
@@ -0,0 +1,1 @@
+import{r as n}from"./UN4s1kDF.js";import a from"./DiDHWqQN.js";import{z as s,H as t,i as e,w as i,W as p}from"./B_y5R0S2.js";import"./BN_7HF1G.js";const d={__name:"ProseWarning",setup(c){const o=s();return(r,f)=>(e(),t(a,{color:"warning",icon:p(o).ui.icons.warning},{default:i(()=>[n(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
