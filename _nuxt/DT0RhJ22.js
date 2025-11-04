17b896b5b25008e5596dedf2d703777497f58647
Olamide-png <otoyinbo@eshopworld.com>
2025-11-04 14:14:46 +0000

    Deploying to gh-pages from @ Olamide-png/docs-test@110bcfc6c81b2950c6b0f3c8a96838fa4d254eb5 🚀

--- _nuxt/DT0RhJ22.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./Cz_oV3-z.js";import e from"./DysjBNkc.js";import{z as n,H as s,i as a,w as i,W as p}from"./ul5C1JEq.js";import"./BN_7HF1G.js";const d={__name:"ProseNote",setup(c){const o=n();return(r,f)=>(a(),s(e,{color:"info",icon:p(o).ui.icons.info},{default:i(()=>[t(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
