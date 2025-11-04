eea8e08629c100cc6af81aa718c046ce390cd915
Olamide-png <otoyinbo@eshopworld.com>
2025-11-04 14:46:40 +0000

    Swap logo file paths in app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -19,8 +19,8 @@ export default defineAppConfig({
     to: '/',
     logo: {
       alt: '',
-      light: '/ESW-Logo-Dark.svg',
-      dark: '/logo.svg'
+      light: '/logo.svg',
+      dark: '/ESW-Logo-Dark.svg'
     },
     search: true,
     colorMode: true,
